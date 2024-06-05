bb71ffd0a3ed20287d1b31b8234d9eb90f581047
Deon Levy <deon.levy@polk-fl.net>
2024-06-04 22:56:18 -0400

    Learning

--- code/3-state-events/start/src/App.jsx
+++ code/3-state-events/start/src/App.jsx
@@ -5,7 +5,7 @@ import IngredientList from './IngredientList'
 
 function App() {
     const initialRecipe = {
-        title: 'Mashed potatoes',
+        title: 'Mashed Potatoes',
         feedback: {
             rating: 4.8,
             reviews: 20
@@ -20,22 +20,36 @@ function App() {
     };
 
     // TODO: Create recipe state
+    const [ recipe, setRecipe ] = useState(initialRecipe);
 
     // TODO: Add new state property
+    const [ prepared, setPrepared ] = useState(false);
 
     // TODO: Create ingredientClick event listener
+    function ingredientClick(index) {
+        const updatedRecipe = { ... recipe };
+        updatedRecipe.ingredients[index].prepared = !updatedRecipe.ingredients[index].prepared;
+        setRecipe(updatedRecipe);
+    }
 
     // TODO: Add the effect hook
+    useEffect(() => {
+        setPrepared(recipe.ingredients.every(i => i.prepared));
+    }, [recipe]);
 
     return (
         <article>
             <h1>Recipe Manager</h1>
 
             {/* TODO: Pass recipe metadata to RecipeTitle */}
+            <RecipeTitle title={recipe.title} feedback={recipe.feedback} />
 
             {/* TODO: Pass ingredients and event listener to IngredientList */}
+            <IngredientList ingredients={recipe.ingredients} onClick={ ingredientClick } />
 
             {/* TODO: Add the prep work display */}
+            { prepared ? <h2>Prep work done!</h2> : <h2>Just keep chopping.</h2>}
+            
         </article>
     )
 }
